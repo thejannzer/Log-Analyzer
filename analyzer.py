d2054385bb65db8cebf420f78dc759dd7eb413dd
thejannzer <scholz.carlsfeld@gmail.com>
2026-04-09 14:17:05 +0200

    Erzeugung eigener Log's für Tests

---
+++ analyzer.py
@@ -0,0 +1,34 @@
+#Log Analyzer in python... Auswertung von INFO, WARNING oder FEHLER - Meldungen
+
+#Erzeugen von eigenen Log's
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+logging.info("App gestartet")
+logging.error("Fehler aufgetreten")
+logging.warning("Achtung!")
+
+def parse_line(line):
+    parts = line.split(" ", 3)
+
+    if len(parts) < 4:
+        return None  # ungültige Zeile ignorieren
+
+    return {
+        "timestamp": parts[0] + " " + parts[1],
+        "level": parts[2],
+        "message": parts[3].strip()
+    }
+
+#Lesen des Log's Zeile für Zeile
+with open("app.log", "r") as file:
+    for line in file:
+        log = parse_line(line)
+        if log:
+            print(log)
+
